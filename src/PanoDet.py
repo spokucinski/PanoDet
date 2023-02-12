42654cf971dd2afb62e4fb09d6e62fd3903908fb
Sebastian Pokuciński <sebastian.pokucinski@gmail.com>
2023-02-12 11:48:30 +0100

    Started work on other submodules.

--- src/PanoDet.py
+++ src/PanoDet.py
@@ -25,9 +25,10 @@ yolov6_train_path = '/home/sebex/Repos/PanoDet/external/YOLOv6/tools/train.py'
 yolov6_val_path = '/home/sebex/Repos/PanoDet/external/YOLOv6/tools/eval.py'
 tested_yolov6_models = ['yolov6n', 'yolov6n6']#['yolov6n', 'yolov6n6', 'yolov6s', 'yolov6s6', 'yolov6m', 'yolov6m6', 'yolov6l', 'yolov6l6']
 
-yolov7_train_path = ''
-yolov7_val_path = ''
-tested_yolov7_models = ['yolov5n', 'yolov5s']
+v7_datasets_path = '../datasets/YOLOv5' # Dataset format for v7 is the same as for v5
+yolov7_train_path = '/home/sebex/Repos/PanoDet/external/YOLOv7/train.py'
+yolov7_val_path = '/home/sebex/Repos/PanoDet/external/YOLOv5/val.py'
+tested_yolov7_models = ['yolov7', 'yolov7x', 'yolov7-w6', 'yolov7-e6', 'yolov7-d6', 'yolov7-e6e']
 
 yolov8_train_path = ''
 yolov8_val_path = ''
@@ -90,34 +91,85 @@ tested_yolov8_models = ['yolov5n', 'yolov5s']
 #                                     ]
 #                         subprocess.Popen(test_cmd, stdout=subprocess.PIPE).wait()
 
-# YOLOv6 Experiments
-v6_datasets_to_test = []
-for root, dirs, files in os.walk(v6_datasets_path):
+# # YOLOv6 Experiments
+# v6_datasets_to_test = []
+# for root, dirs, files in os.walk(v6_datasets_path):
+#     for file in files:
+#         if file.endswith(".yaml"):
+#             v6_datasets_to_test.append(os.path.join(root, file))
+#
+# for tested_dataset in v6_datasets_to_test:
+#     # YOLOv6 Experiments
+#     for tested_model in tested_yolov6_models:
+#         for image_size in image_sizes:
+#             for epoch_size in epochs:
+#                 for batch_size in batch_sizes:
+#
+#                     # YOLOv6 does not support auto batch size
+#                     if batch_size == -1:
+#                         continue
+#
+#                     # Split the path to datset with use of os separator, take the last separated element ('datasetname.yaml')
+#                     # split it again with the dot and take the first part - name of the .yaml file ('datasetname')
+#                     tested_dataset_name = tested_dataset.split(os.sep)[-1].split('.')[0]
+#                     run_name = f'{tested_model}_{image_size}_{epoch_size}_{batch_size}'
+#
+#                     if not os.path.exists(os.path.join(results_path, 'YOLOv6', tested_dataset_name, 'Train')):
+#                         os.makedirs(os.path.join(results_path, 'YOLOv6', tested_dataset_name, 'Train'))
+#                     done_trainings = os.listdir(os.path.join(results_path, 'YOLOv6', tested_dataset_name, 'Train'))
+#                     if run_name not in done_trainings:
+#                         train_cmd = ['python', f'{yolov6_train_path}',
+#                                      f'--output-dir={results_path}/YOLOv6/{tested_dataset_name}/Train',
+#                                      f'--name={run_name}',
+#                                      f'--data-path={tested_dataset}',
+#                                      f'--epochs={epoch_size}',
+#                                      f'--batch-size={batch_size}',
+#                                      f'--img-size={image_size}',
+#                                      f'--conf-file=../external/YOLOv6/configs/{tested_model}.py']
+#                         subprocess.Popen(train_cmd, stdout=subprocess.PIPE).wait()
+#
+#                     if not os.path.exists(os.path.join(results_path, 'YOLOv6', tested_dataset_name, 'Test')):
+#                         os.makedirs(os.path.join(results_path, 'YOLOv6', tested_dataset_name, 'Test'))
+#                     done_tests = os.listdir(os.path.join(results_path, 'YOLOv6', tested_dataset_name, 'Test'))
+#                     if run_name not in done_tests:
+#                         best_model = os.path.join(results_path, 'YOLOv6', tested_dataset_name, 'Train', run_name,
+#                                                   'weights', 'best_ckpt.pt')
+#                         test_cmd = ['python',
+#                                     yolov6_val_path,
+#                                     f'--task=val',
+#                                     f'--save_dir={results_path}/YOLOv6/{tested_dataset_name}/Test',
+#                                     f'--name={run_name}',
+#                                     f'--data={tested_dataset}',
+#                                     f'--img={image_size}',
+#                                     f'--weights={best_model}',
+#                                     # '--save-txt'
+#                                     ]
+#                         subprocess.Popen(test_cmd, stdout=subprocess.PIPE).wait()
+
+# YOLOv7 Experiments
+v7_datasets_to_test = []
+for root, dirs, files in os.walk(v7_datasets_path):
     for file in files:
         if file.endswith(".yaml"):
-            v6_datasets_to_test.append(os.path.join(root, file))
+            v7_datasets_to_test.append(os.path.join(root, file))
 
-for tested_dataset in v6_datasets_to_test:
+for tested_dataset in v7_datasets_to_test:
     # YOLOv6 Experiments
-    for tested_model in tested_yolov6_models:
+    for tested_model in tested_yolov7_models:
         for image_size in image_sizes:
             for epoch_size in epochs:
                 for batch_size in batch_sizes:
-
-                    # YOLOv6 does not support auto batch size
-                    if batch_size == -1:
-                        continue
 
                     # Split the path to datset with use of os separator, take the last separated element ('datasetname.yaml')
                     # split it again with the dot and take the first part - name of the .yaml file ('datasetname')
                     tested_dataset_name = tested_dataset.split(os.sep)[-1].split('.')[0]
                     run_name = f'{tested_model}_{image_size}_{epoch_size}_{batch_size}'
 
-                    if not os.path.exists(os.path.join(results_path, 'YOLOv6', tested_dataset_name, 'Train')):
-                        os.makedirs(os.path.join(results_path, 'YOLOv6', tested_dataset_name, 'Train'))
-                    done_trainings = os.listdir(os.path.join(results_path, 'YOLOv6', tested_dataset_name, 'Train'))
+                    if not os.path.exists(os.path.join(results_path, 'YOLOv7', tested_dataset_name, 'Train')):
+                        os.makedirs(os.path.join(results_path, 'YOLOv7', tested_dataset_name, 'Train'))
+                    done_trainings = os.listdir(os.path.join(results_path, 'YOLOv7', tested_dataset_name, 'Train'))
                     if run_name not in done_trainings:
-                        train_cmd = ['python', f'{yolov6_train_path}',
+                        train_cmd = ['python', f'{yolov7_train_path}',
                                      f'--output-dir={results_path}/YOLOv6/{tested_dataset_name}/Train',
                                      f'--name={run_name}',
                                      f'--data-path={tested_dataset}',

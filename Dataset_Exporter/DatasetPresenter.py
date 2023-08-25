459d78b02992f5d91cee8073920b279180102c9a
Sebastian Pokuciński <sebastian.pokucinski@gmail.com>
2023-08-25 12:52:26 +0200

    Updated the FiftyOne exporting application to use locally referenced packages and modules.

--- Dataset_Exporter/DatasetPresenter.py
+++ Dataset_Exporter/DatasetPresenter.py
@@ -5,31 +5,38 @@ import shutil
 import fiftyone.utils.random as four
 
 from Exporter import CSVImageClassificationDatasetExporter
+os.chdir("Dataset_Exporter")
 
-name = f"CODE3K"
-dataset_type = fo.types.CVATImageDataset
-export_dataset_type = fo.types.TFImageClassificationDataset
-DATA_PATH = "C:\CODE\Dataset\DataSources"
-EXPORT_PATH = "C:\CODE\Dataset\Export"
+FO_DATASET_NAME = f"Red_House_10C"
+IMPORT_TYPE = fo.types.CVATImageDataset
+EXPORT_TYPE = fo.types.TFImageClassificationDataset
+DATA_PATH = "data\DataSources"
+EXPORT_PATH = "results"
+RUN_FO_SESSION = False
+
 data_path_exists = os.path.exists(DATA_PATH)
-
 existing_datasets = fo.list_datasets()
-dataset = fo.Dataset(name=name)
+dataset = fo.Dataset(name=FO_DATASET_NAME)
 
 data_sources = os.listdir(DATA_PATH)
+data_sources = [datasource for datasource in data_sources if os.path.isdir(os.path.join(DATA_PATH, datasource))]
 for data_source in data_sources:
     subset_images_path = os.path.join(DATA_PATH, data_source, "images")
     subset_ann_path = os.path.join(DATA_PATH, data_source, "annotations.xml")
-    dataset.merge_dir(data_path=subset_images_path, labels_path=subset_ann_path, dataset_type=dataset_type)
+    dataset.merge_dir(data_path=subset_images_path, labels_path=subset_ann_path, dataset_type=IMPORT_TYPE)
 
-# session = fo.launch_app(dataset)
-# session.wait()
+if RUN_FO_SESSION:
+    session = fo.launch_app(dataset)
+    session.wait()
 
-four.random_split(dataset, {"train": 0.7, "val": 0.15, "test": 0.15})
+four.random_split(dataset, {"train": 0.9, "val": 0, "test": 0.1})
+print(dataset.count_sample_tags())
+
 train_dataset = dataset.match_tags("train")
 val_dataset = dataset.match_tags("val")
 test_dataset = dataset.match_tags("test")
 
+print(f"Processing train dataset. Count of elements: {len(train_dataset)}")
 for sample in train_dataset:
     try:
         label = sample["classifications"]["classifications"][0]["label"]
@@ -41,6 +48,7 @@ for sample in train_dataset:
     except:
         continue
 
+print(f"Processing val dataset. Count of elements: {len(val_dataset)}")
 for sample in val_dataset:
     try:
         label = sample["classifications"]["classifications"][0]["label"]
@@ -52,6 +60,7 @@ for sample in val_dataset:
     except:
         continue
 
+print(f"Processing test dataset. Count of elements: {len(test_dataset)}")
 for sample in test_dataset:
     try:
         label = sample["classifications"]["classifications"][0]["label"]
@@ -63,12 +72,4 @@ for sample in test_dataset:
     except:
         continue   
 
-
-# sample = dataset.first()
-# classifications = sample["classifications"]
-# internal_classifications = classifications["classifications"]
-# actual_classification = internal_classifications[0]["label"]
-
-# dataset.export(export_dir=EXPORT_PATH, dataset_type=export_dataset_type)
-
-i = 5 
+print("Processing ended!")

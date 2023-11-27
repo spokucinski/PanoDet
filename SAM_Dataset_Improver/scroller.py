06633682e855eb732ac59b5c0da43402389d21b8
Sebastian Pokuciński <sebastian.pokucinski@gmail.com>
2023-11-27 21:16:20 +0100

    Improved the scroller imlementation to combined switching to next image from a specific folder.

--- SAM_Dataset_Improver/scroller.py
+++ SAM_Dataset_Improver/scroller.py
@@ -1,25 +1,57 @@
 import cv2
+import numpy as np
+import glob
+import os
 
-global mouseX,mouseY
+MAIN_WIN = "main"
+RES_WIN = "result"
+INPUT_PATH = "input"
 
-def draw_circle(event,x,y,flags,param):   
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        cv2.circle(img,(x,y),100,(255,0,0),-1)
-        mouseX,mouseY = x,y
+res = []
+for root, dirs, files in os.walk(INPUT_PATH):
+    for file in files:
+        if file.lower().endswith(".jpg") or file.lower().endswith(".png"):
+            res.append(os.path.join(root, file))
 
-def draw_line(event,x,y,flags,param):
+loaded_image_index = 0
+
+original_img = cv2.imread(res[loaded_image_index])
+original_img_path = ""
+marked_img = original_img.copy()
+last_known_x = 0
+line_thickness = 3
+loaded_image_index = 1
+
+def split_image(event, x, y, flags, param):
+    global original_img, marked_img, last_known_x, loaded_image_index
+
     if event == cv2.EVENT_MOUSEMOVE:
-        i = 4
-        
-img = cv2.imread("C:\\Users\\Admin\\Downloads\\Bedroom.jpg")
+        marked_img[:, last_known_x-line_thickness:last_known_x+line_thickness] = original_img[:, last_known_x-line_thickness:last_known_x+line_thickness]
+        last_known_x = x
+        cv2.line(marked_img, (x, 0), (x, marked_img.shape[0]), (0, 0, 255), line_thickness)
 
-cv2.namedWindow('image')
-cv2.setMouseCallback('image',draw_circle)
+    elif event == cv2.EVENT_LBUTTONDOWN:
+        marked_img = original_img.copy()
+        left_part = marked_img[0:marked_img.shape[0], 0:x]
+        right_part = marked_img[0:marked_img.shape[0], x:marked_img.shape[1]]
+        combined = np.concatenate((right_part, left_part), axis=1)
+        cv2.namedWindow(RES_WIN, cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(RES_WIN, 1800, 900)
+        cv2.imshow(RES_WIN, combined)
 
-while(1):
-    cv2.imshow('image',img)
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        original_img = cv2.imread(res[loaded_image_index])
+        marked_img = original_img.copy()
+        loaded_image_index = loaded_image_index + 1
+
+cv2.namedWindow(MAIN_WIN, cv2.WINDOW_NORMAL)
+cv2.resizeWindow(MAIN_WIN, 1800, 900)
+cv2.setMouseCallback(MAIN_WIN, split_image)
+
+
+
+while (1):
+    cv2.imshow(MAIN_WIN, marked_img)
     k = cv2.waitKey(20) & 0xFF
     if k == 27:
         break
-    elif k == ord('a'):
-        print (mouseX, mouseY)

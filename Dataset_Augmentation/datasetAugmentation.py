811e4705f0dd04cfdf05a66a7f0202a028c9bd87
Sebastian Pokuciński <sebastian.pokucinski@gmail.com>
2024-01-18 14:48:48 +0100

    Implemented first version of full dataset augmentation - images and annotations.

--- Dataset_Augmentation/datasetAugmentation.py
+++ Dataset_Augmentation/datasetAugmentation.py
@@ -4,12 +4,26 @@ import albumentations as A
 import os
 from matplotlib import pyplot as plt
 
+class Annotation():
+
+    def __init__(self,
+                 objectType: int,
+                 xCenter: float,
+                 yCenter: float,
+                 width: float,
+                 height: float):
+        
+        self.objectType = objectType
+        self.xCenter = xCenter
+        self.yCenter = yCenter
+        self.width = width
+        self.height = height
+
 # Define the augmentation pipeline
 transform = A.Compose([
     A.HorizontalFlip(p=0.5),  # Horizontal flip with 50% probability
     A.RandomBrightness(0.9, True, 1)
- ])
-# ), bbox_params=A.BboxParams(format='yolo'))
+ ], bbox_params=A.BboxParams(format='yolo'))
 
 # Define your dataset directory and image list
 datasetDir = 'datasets'
@@ -54,7 +68,30 @@ for dataset in datasets:
         image = cv2.imread(imagePath)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        augmentedImage = transform(image=image)
+        annotations: list[Annotation] = []
+        with open(imagePath.replace("images", "labels").replace(".jpg", ".txt").replace(".png", ".txt"), 'r') as reader:                 
+            for annotation in reader.readlines():
+                objectType, xCenter, yCenter, width, height = annotation.split(' ')
+                annotations.append(Annotation(int(objectType), float(xCenter), float(yCenter), float(width), float(height)))
+
+        bboxes = []
+        for annotation in annotations:
+            bboxes.append([annotation.xCenter, annotation.yCenter, annotation.width, annotation.height, str(annotation.objectType)])
+
+        augmentedImage = transform(image=image, bboxes=bboxes)
         resultsPath = imagePath.replace('datasets', 'augmentedDatasets')
         os.makedirs(os.path.dirname(resultsPath), exist_ok=True)
+        augmentedAnnotations: list[Annotation] = []
+        for bbox in augmentedImage['bboxes']:
+            augmentedAnnotations.append(Annotation(int(bbox[4]), bbox[0], bbox[1], bbox[2], bbox[3]))
+        
         cv2.imwrite(resultsPath, augmentedImage['image'])
+        resultsPath = resultsPath.replace("images", "labels").replace(".jpg", ".txt").replace(".png", ".txt")
+        os.makedirs(os.path.dirname(resultsPath), exist_ok=True)
+        with open(resultsPath, "w") as writer:
+            writer.writelines(
+                list(
+                    map(
+                        lambda postProcessedAnnotation: 
+                            f"{postProcessedAnnotation.objectType} {postProcessedAnnotation.xCenter} {postProcessedAnnotation.yCenter} {postProcessedAnnotation.width} {postProcessedAnnotation.height}\n", 
+                            augmentedAnnotations)))

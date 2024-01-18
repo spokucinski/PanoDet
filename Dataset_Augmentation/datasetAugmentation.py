dd0d8c664e21ffbe0ab8ffdba672e16b9cf9f8b8
Sebastian Pokuciński <sebastian.pokucinski@gmail.com>
2024-01-18 14:04:32 +0100

    First working augmentation pipeline with use of Albumentations.

--- Dataset_Augmentation/datasetAugmentation.py
+++ Dataset_Augmentation/datasetAugmentation.py
@@ -4,6 +4,12 @@ import albumentations as A
 import os
 from matplotlib import pyplot as plt
 
+# Define the augmentation pipeline
+transform = A.Compose([
+    A.HorizontalFlip(p=0.5),  # Horizontal flip with 50% probability
+    A.RandomBrightness(0.9, True, 1)
+ ])
+# ), bbox_params=A.BboxParams(format='yolo'))
 
 # Define your dataset directory and image list
 datasetDir = 'datasets'
@@ -28,7 +34,7 @@ for dataset in datasets:
                 
                 for image in images:
                     if image.lower().endswith(tuple([".jpg", ".png"])):
-                        imagePaths.append(os.path.join(dataset, datasetDirectory, image))
+                        imagePaths.append(os.path.join(dataset, datasetDirectory, imagesDirectory, image))
             
             if datasetDirectory.lower() == "labels":
                 directoryPath = os.path.join(dataset, datasetDirectory)
@@ -38,56 +44,17 @@ for dataset in datasets:
                 
                 for annotationFile in annotationFile:
                     if annotationFile.lower().endswith(".txt"):
-                        annotationPaths.append(os.path.join(dataset, datasetDirectory, annotationFile))
+                        annotationPaths.append(os.path.join(dataset, datasetDirectory, annotationsDirectory, annotationFile))
         
     foundImagesCount: int = len(imagePaths)
     foundAnnotationFilesCounts: int = len(annotationPaths)
     print(f"Found: {foundImagesCount} images and {foundAnnotationFilesCounts} annotation files!")
 
-    
+    for imagePath in imagePaths:
+        image = cv2.imread(imagePath)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-
-
-
-
-# Define the augmentation pipeline
-transform = A.Compose([
-    A.Resize(416, 416),  # Resize your images to the desired size
-    A.HorizontalFlip(p=0.5),  # Horizontal flip with 50% probability
-    # Add more augmentations as needed
-    ToTensorV2(),  # Convert the image and annotations to PyTorch tensors
-], bbox_params=A.BboxParams(format='yolo'))
-
-
-for image_file in image_list:
-    # Load the image
-    image_path = os.path.join(datasetDir, 'images', 'AutomaticallyMinScrolledDataset', image_file)
-    image = cv2.imread(image_path)
-    
-    # Load the YOLO annotation file
-    annotation_path = os.path.join(datasetDir, 'labels', 'AutomaticallyMinScrolledDataset', image_file.replace('.jpg', '.txt'))
-    
-    # Read the content of the annotation file
-    with open(annotation_path, 'r') as f:
-        lines = f.readlines()
-    
-    bboxes = []
-    class_labels = []
-    
-    for line in lines:
-        values = line.strip().split()
-        class_id = int(values[0])
-        x_center, y_center, width, height = map(float, values[1:])
-        x_min = (x_center - width / 2)
-        y_min = (y_center - height / 2)
-        x_max = (x_center + width / 2)
-        y_max = (y_center + height / 2)
-        bboxes.append([x_min, y_min, x_max, y_max])
-        class_labels.append(class_id)
-    
-    # Apply augmentations
-    augmented = transform(image=image, bboxes=bboxes)
-    
-    augmented = transform(image=image, bboxes=bboxes)
-    augmented_image = augmented['image']
-    augmented_bboxes = augmented['bboxes']
+        augmentedImage = transform(image=image)
+        resultsPath = imagePath.replace('datasets', 'augmentedDatasets')
+        os.makedirs(os.path.dirname(resultsPath), exist_ok=True)
+        cv2.imwrite(resultsPath, augmentedImage['image'])

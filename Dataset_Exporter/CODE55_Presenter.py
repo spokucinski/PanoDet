e73245af5c2f15a576f772ecf9b53a3cdd7510db
Sebastian Pokuciński <sebastian.pokucinski@gmail.com>
2024-04-18 14:44:23 +0200

    Fixed heatmap coordinates marking.

--- Dataset_Exporter/CODE55_Presenter.py
+++ Dataset_Exporter/CODE55_Presenter.py
@@ -102,23 +102,24 @@ print(dataset.bounds(gt_areas))
 print(dataset.mean(gt_areas))
 
 masterHeatmap = np.zeros(shape=(1024, 2048))
-for i in range(3):
-    for sample in dataset:
-        partialMap = np.zeros(shape=(1024, 2048))
-        sampleImage = cv2.imread(sample.filepath)
-        
-        for detection in sample.detections.detections:
-            xmin = int(detection.bounding_box[0] * sample.metadata.width)
-            xmax = int((detection.bounding_box[0] + detection.bounding_box[2]) * sample.metadata.width)
 
-            ymin = int(detection.bounding_box[1] * sample.metadata.height)
-            ymax = int((detection.bounding_box[1] + detection.bounding_box[3]) * sample.metadata.height)
-                
-            partialMap[xmin:xmax, ymin:ymax] += 1
+for sample in dataset.take(3):
+    partialMap = np.zeros(shape=(1024, 2048))
+    sampleImage = cv2.imread(sample.filepath)
+    sampleImage = cv2.resize(sampleImage, (2048, 1024))
+    cv2.imwrite("image.png", sampleImage)
+    for detection in sample.detections.detections:
+        xmin = int(detection.bounding_box[0] * 2048)
+        xmax = int((detection.bounding_box[0] + detection.bounding_box[2]) * 2048)
 
-            normalizedMap = np.interp(partialMap, (partialMap.min(), partialMap.max()), (0, 1))
-            normalizedMap = np.multiply(normalizedMap, 255)
-            cv2.imwrite("partialMapPreview.png", normalizedMap)
+        ymin = int(detection.bounding_box[1] * 1024)
+        ymax = int((detection.bounding_box[1] + detection.bounding_box[3]) * 1024)
+            
+        partialMap[ymin:ymax, xmin:xmax] += 1
+
+        normalizedMap = np.interp(partialMap, (partialMap.min(), partialMap.max()), (0, 1))
+        normalizedMap = np.multiply(normalizedMap, 255)
+        cv2.imwrite("partialMapPreview.png", normalizedMap)
 
 
 mapa = np.random.randint(256, size=(128, 128), dtype=np.uint8)
